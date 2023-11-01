1da783c118fb59615721bfb1ad3c78d10bf29e64
mualal <almuravcev@yandex.ru>
2023-11-01 23:50:46 +0300

    lattice-lattice: set periodic boundary conditions and add new wave shape

--- matlab/lattice_lattice_interface.m
+++ matlab/lattice_lattice_interface.m
@@ -1,130 +1,137 @@
 clear;
 
+% time step to save disp
+save_time = 1;
+
+% lattice geometry
 left_x = -100;
-right_x = 150;
+right_x = 100;
 left_y = -100;
 right_y = 100;
 
+% parameters
 m_1 = 1;
 m_2 = 0.5;
 c = 0.75;
 omega = 1;
-a = 1;
+a = 2;
 
 k_1 = asin(omega/2 * sqrt(m_1/c)) * 2 / a;
 
-particles_num_x = left_x:a:right_x;
-particles_num_y = left_y:a:right_y;
+num_x = left_x:a:right_x;
+num_y = left_y:a:right_y;
 
-particles_mass = zeros(int32((-left_y+right_y+1)/a),int32((-left_x+right_x+1)/a));
-for j=1:length(particles_num_x)
-    for i=1:length(particles_num_y)
-%        if j < length(particles_num_x)/2
-        if j < -left_x
-            particles_mass(i,j) = m_1;
+mass = zeros(int32((-left_y+right_y)/a),int32((-left_x+right_x)/a));
+for j=1:length(num_x)
+    for i=1:length(num_y)
+        if j < -left_x / a
+            mass(i,j) = m_1;
         else
-            particles_mass(i,j) = m_2;
+            mass(i,j) = m_2;
         end
     end
 end
 
 angle_step = 3;
-%for j=int32(length(particles_num_x)/2):length(particles_num_x)
-for j=-left_x:length(particles_num_x)
-    for i=angle_step:length(particles_num_y)
-        particles_mass(i,j) = m_1;
+for j=-int32(left_x/a):length(num_x)
+    for i=angle_step:length(num_y)
+        mass(i,j) = m_1;
     end
     angle_step = angle_step + 3;
 end
 
-particles_disp = zeros(int32((-left_y+right_y+1)/a),int32((-left_x+right_x+1)/a));
-particles_vel = zeros(int32((-left_y+right_y+1)/a),int32((-left_x+right_x+1)/a));
+% interface coordinates finder
+x_coord = zeros(1,length(num_y)-1);
+y_coord = zeros(1,length(num_y)-1);
+temp_count=1;
+for i=1:length(num_y)
+    for j=1:length(num_x)-1
+        if mass(i,j) ~= mass(i,j+1)
+            x_coord(temp_count) = j*a+left_x;
+            y_coord(temp_count) = (i-1)*a+left_y;   
+        end
+    end
+    temp_count = temp_count + 1;
+end
 
-beta = 0.1;
+disp = zeros(int32((-left_y+right_y)/a),int32((-left_x+right_x)/a));
+vel = zeros(int32((-left_y+right_y)/a),int32((-left_x+right_x)/a));
+
+beta_x = 0.07;
+beta_y = 0.07;
 n_0 = -35;
 u_0 = 1;
 g_1 = a * sqrt(c/m_1) * cos(k_1*a/2);
 
-for i=1:length(particles_num_x)
-    for j=1:length(particles_num_y)
-        if i < length(particles_num_x)
-            particles_disp(j, i) = u_0 * exp(-beta^2/2 * (particles_num_x(i) - n_0)^2) * sin(particles_num_x(i) * k_1);
-            particles_vel(j, i) = -u_0 * exp(-beta^2/2 * (particles_num_x(i) - n_0)^2) * (omega * cos(k_1*(particles_num_x(i))) - beta^2*g_1/a*(particles_num_x(i)-n_0)*sin(particles_num_x(i) * k_1));
-        end
+for i=1:length(num_x)
+    for j=1:length(num_y)
+        disp(j, i) = u_0 * exp(-beta_x^2/2 * (num_x(i) - n_0)^2) * exp(-beta_y^2/2 * (num_y(j))^2);
+        disp(j, i) = disp(j, i) * sin(num_x(i) * k_1);
+        vel(j, i) = -u_0 * exp(-beta_x^2/2 * (num_x(i) - n_0)^2) * exp(-beta_y^2/2 * (num_y(j))^2);
+        vel(j, i) = vel(j, i) * (omega * cos(k_1*(num_x(i))) - beta_x^2*g_1/a*(num_x(i)-n_0)*sin(num_x(i) * k_1));
     end
 end
 
 figure; hold on
-[X,Y] = meshgrid(particles_num_x,particles_num_y);
-s1 = surf(X,Y,particles_disp,'FaceAlpha',0.9);
-s2 = surf(X,Y,particles_mass,'FaceAlpha',0.5);
+[X,Y] = meshgrid(num_x,num_y);
+surf(X,Y,disp,'FaceAlpha',0.9,'EdgeAlpha',0.5);
+plot3(x_coord,y_coord,max(max(disp))*ones(1,length(x_coord)),'LineWidth',2,'Color','Black')
 title('Цветовая карта перемещений в момент времени t = 0');
 xlabel('Номер частицы по оси Ox');
 ylabel('Номер частицы по оси Oy');
 colorbar;
-s1.EdgeColor = 'none';
-s2.EdgeColor = 'none';
 view(17,22);
 hold off
 
 
-dt = 0.001;
+dt = 0.01;
 t_max = 150;
 times = 0:dt:t_max;
 
+result = cell(1, fix(t_max/save_time));
+
 for t=times
-    for ind_x=1:length(particles_num_x)
-        for ind_y=1:length(particles_num_y)
-            if ind_x ~= 1 && ind_y ~= 1 && ind_x ~= length(particles_num_x) && ind_y ~= length(particles_num_y)
-                particles_vel(ind_y,ind_x) = particles_vel(ind_y,ind_x) + c/particles_mass(ind_y, ind_x)*(particles_disp(ind_y-1, ind_x)+particles_disp(ind_y+1, ind_x)+particles_disp(ind_y, ind_x-1)+particles_disp(ind_y, ind_x+1)-4*particles_disp(ind_y, ind_x))*dt;
-                particles_disp(ind_y, ind_x) = particles_disp(ind_y, ind_x) + particles_vel(ind_y, ind_x) * dt;
+    for ind_x=1:length(num_x)
+        for ind_y=1:length(num_y)
+            ix_m = ind_x-1;
+            ix_p = ind_x+1;
+            iy_m = ind_y-1;
+            iy_p = ind_y+1;
+            if ind_x == 1
+                ix_m = length(num_x);
+            end
+            if ind_x == length(num_x)
+                ix_p = 1;
             end
             if ind_y == 1
-                particles_vel(ind_y,ind_x) = particles_vel(ind_y+1,ind_x);
-                particles_disp(ind_y,ind_x) = particles_disp(ind_y+1,ind_x);
+                iy_m = length(num_y);
             end
-            if ind_y == length(particles_num_y)
-                particles_vel(ind_y,ind_x) = particles_vel(ind_y-1,ind_x);
-                particles_disp(ind_y,ind_x) = particles_disp(ind_y-1,ind_x);
+            if ind_y == length(num_y)
+                iy_p = 1;
             end
+            vel(ind_y,ind_x) = vel(ind_y,ind_x) + c/mass(ind_y, ind_x)*(disp(iy_m, ind_x)+disp(iy_p, ind_x)+disp(ind_y, ix_m)+disp(ind_y, ix_p)-4*disp(ind_y, ind_x))*dt;
+            disp(ind_y, ind_x) = disp(ind_y, ind_x) + vel(ind_y, ind_x) * dt;
         end
     end
-    if t==60
-        figure; hold on
-        s1 = surf(X,Y,particles_disp,'FaceAlpha',0.9);
-        s2 = surf(X,Y,particles_mass,'FaceAlpha',0.5);
-        title('Цветовая карта перемещений в момент времени t = 60');
-        xlabel('Номер частицы по оси Ox');
-        ylabel('Номер частицы по оси Oy');
-        colorbar;
-        s1.EdgeColor = 'none';
-        s2.EdgeColor = 'none';
-        view(17,22);
-        hold off
-    end
-    if t==120
-        figure; hold on
-        s1 = surf(X,Y,particles_disp,'FaceAlpha',0.9);
-        s2 = surf(X,Y,particles_mass,'FaceAlpha',0.5);
-        title('Цветовая карта перемещений в момент времени t = 120');
-        xlabel('Номер частицы по оси Ox');
-        ylabel('Номер частицы по оси Oy');
-        colorbar;
-        s1.EdgeColor = 'none';
-        s2.EdgeColor = 'none';
-        view(17,22);
-        hold off
-    end
-        
+    if rem(t, save_time) == 0
+        result{fix(t/save_time)+1} = disp;
+    end     
 end
+
 figure; hold on
-s1 = surf(X,Y,particles_disp,'FaceAlpha',0.9);
-s2 = surf(X,Y,particles_mass,'FaceAlpha',0.5);
-title('Цветовая карта перемещений в момент времени t = 150');
+s1 = surf(X,Y,disp,'FaceAlpha',0.9,'EdgeAlpha',0.7);
+title('Цветовая карта перемещений в момент времени t = '+string(t_max));
 xlabel('Номер частицы по оси Ox');
 ylabel('Номер частицы по оси Oy');
+l1 = plot3(x_coord,y_coord,10*ones(1,length(x_coord)),'LineWidth',2,'Color','Black');
 colorbar;
-s1.EdgeColor = 'none';
-s2.EdgeColor = 'none';
 view(17,22);
+for i = 1:length(result)
+    title('Цветовая карта перемещений в момент времени t = '+string((i-1)*save_time));
+    s1.XData = X;
+    s1.YData = Y;
+    s1.ZData = result{i};
+    l1.ZData = max(max(result{i}))*ones(1,length(x_coord));
+    pause(0.2)
+end
 hold off
